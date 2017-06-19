129d8dee445d1fee66983fd1cf841fef62e8f25e
Sasha Goldenson <sgoldens@gmail.com>
2017-06-18 19:40:39 -0700

    GeoMap component, nonfunctional commit to switch work stations.

---
+++ map-game/src/components/GeoMap.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class GeoMap extends Component {
+  render() {
+    return (
+      <div className="GeoMap">
+        <div id="map">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default GeoMap;
