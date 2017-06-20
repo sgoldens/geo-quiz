120284e8c1f23d3a2dfdb6f33ce5b291715f807d
Sasha Goldenson <sgoldens@gmail.com>
2017-06-20 11:37:11 -0700

    Place a Marker and center map on a random country.

--- map-game/src/components/Map.js
+++ map-game/src/components/Map.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
-import { withGoogleMap, GoogleMap } from "react-google-maps";
+import { withGoogleMap, GoogleMap, Marker, Overlay } from "react-google-maps";
 
-import mapStyles from "./mapStyles.json";
+import mapStylesWithoutLabels from "./mapStylesWithoutLabels.json";
+// import mapStylesWithLabels1 from "./mapStylesWithLabels1.json";
+
+import { COUNTRIES } from './countries.js';
+
+function getRandomInt (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+/* Choose a random country */
+let randomCountry = COUNTRIES[getRandomInt(0,244)];
+/* Pull out the { Latitude, Longitude } as an object, and Name as a string */
+let randomCountryLatLong = { lat: randomCountry[1], lng: randomCountry[2] }
+let randomCountryName = randomCountry[4]
+console.log("Random Country: " + randomCountry)
 
 const GeoGoogleMap = withGoogleMap(props => (
   <GoogleMap
     ref={props.onMapLoad}
-    defaultZoom={2}
-    defaultCenter={{ lat: 42.4242424, lng: -42.4242424 }}
-    defaultOptions={{ styles: mapStyles }}
+    defaultZoom={4}
+    defaultCenter={ randomCountryLatLong }
+    defaultOptions={{ styles: mapStylesWithoutLabels }}
     onClick={props.onMapClick}
   >
+    <Marker
+      position={randomCountryLatLong}
+      key={randomCountryName}
+    />
   </GoogleMap>
 ));
 
@@ -22,16 +40,15 @@ export default class Map extends Component {
     if (map) {
       console.log("Zoom: " + map.getZoom());
     }
-
-    const makeRequest = function() {
-      displayRandomCountry();
-    };
-
-    function displayRandomCountry() {
-      console.log('inside displayRandomCountry()')
-      /* display a random country */
-    }
-    makeRequest();
+    //
+    // const makeRequest = function() {
+    //   displayRandomCountry();
+    // };
+    //
+    // function displayRandomCountry() {
+    //   /* diplay a list of five options, four wrong, one correct, in an Overlay  */
+    // }
+    // makeRequest();
   }
 
   render() {
